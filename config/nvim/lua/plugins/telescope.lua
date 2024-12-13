3482280a38474c2f577d0fa05b1023b34f01c259
marc <marc.eiro@gmx.com>
2024-12-13 13:17:05 +0100

    allow telescope search to find hidden files

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -7,13 +7,13 @@ return {
 			pickers = {
 				find_files = {
 					follow = true, -- allows to find simlinked files
+					hidden = true, -- allows to find hidden files
 				},
 			},
 		})
 
 		-- set keymaps
 		local keymap = vim.keymap
-
 		keymap.set("n", "<leader>fs", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find string in cwd" })
